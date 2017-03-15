07b9299caeab1c99e16729a985bd7c1ef14c6861
liugeng <liugeng15@163.com>
2017-03-15 14:05:16 +0800

    add babel-polyfill

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import reducer from './reducers'
 import App from './components/TodoApp'
 import 'todomvc-common/base.css'
 import 'todomvc-app-css/index.css'
+import 'babel-polyfill';
 
 let store = createStore(reducer);
 
